8cd43f7ffb86d08bccd35667a589ca5eb585c805
Charles Thompson <mail@kinesis.me>
2013-04-29 08:32:09 -0700

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -4,7 +4,7 @@
 # chroot.sh v1.0.0 - kinesis (mail@kinesis.me)                                 #
 # Objective: complete and concisive method of chrooting processes in RHEL      #
 #                                                                              #
-# Copyright (c) 2012 Charles Thompson, all rights reserved.                    #
+#    Written by Charles Thompson (mail@kinesis.me)                             #
 #    This program is free software: you can redistribute it and/or modify      #
 #    it under the terms of the GNU General Public License as published by      #
 #    the Free Software Foundation, either version 3 of the License, or         #
